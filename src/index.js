985bcc9f0c74f13125ee0a3842bf15d69f3a527e
Luiza Sntg <lu.sntgo@gmail.com>
2022-01-23 16:51:01 -0300

    feat: return the last 10 tweets

--- src/index.js
+++ src/index.js
@@ -6,16 +6,34 @@ app.use(json());
 app.use(cors());
 
 let tweets = [];
-let user = [];
+let user = {};
 let avatar = [];
 const login = [];
 
 app.post("/sign-up", (req, res) => {
   const username = req.body;
   login.push(username);
-  res.send(username);
+  res.send("Ok");
 });
 
+app.post("/tweets", (req, res) => {
+  const tweet = {
+    avatar: user.avatar,
+    username: req.body.username,
+    tweet: req.body.tweet,
+  };
+  tweets.push(tweet);
+  console.log(user);
+  res.send("OKK");
+});
+
+app.get("/tweets", (req, res) => {
+  if (tweets.length <= 10) {
+    res.send(tweets);
+  } else {
+    res.send(tweets.slice(tweets.length - 10, tweets.length));
+  }
+});
 app.listen(5000, () => {
   console.log("Funciona");
 });

aeb23b75a34db831b0a5be5c825cb6057e1e0316
Luiza Sntg <lu.sntgo@gmail.com>
2022-01-23 16:14:57 -0300

    feat: user sign up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import express from "express";
+import express, { json } from "express";
+import cors from "cors";
 
 const app = express();
+app.use(json());
+app.use(cors());
+
+let tweets = [];
+let user = [];
+let avatar = [];
+const login = [];
+
+app.post("/sign-up", (req, res) => {
+  const username = req.body;
+  login.push(username);
+  res.send(username);
+});
 
 app.listen(5000, () => {
   console.log("Funciona");
